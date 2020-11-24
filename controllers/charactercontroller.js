adbb6e6de8941ae7fd836af2d7452bbaeabdb138
Derek Black <derekblack94@yahoo.com>
2020-11-24 10:57:03 -0500

    dereksPush2

--- controllers/charactercontroller.js
+++ controllers/charactercontroller.js
@@ -4,6 +4,8 @@ const {Characeter} = require('../models/character')
 const validateSession = require('../middleware/validateSession')
 
 
+//**GET ALL */
+
 router.get("/", (req, res) =>{
     Characeter.findAll()
     .then(characeter => res.status(200).json(characeter))
@@ -12,8 +14,31 @@ router.get("/", (req, res) =>{
     }))
 })
 
+//**GET BY NAME */
+
 router.get('/:characterName', (req, res) => {
     Characeter.findOne({ where: { characeter: req.params.characeter }})
       .then(characeter => res.status(200).json(characeter))
       .catch(err => res.status(500).json({ error: err}))
 })
+
+//** DELETE */
+
+/*router.delete('/delete/:id', (req, res) =>{
+    Characeter.destroy({
+        where: { id: req.params.id}
+    })
+    .then(characeter => res.status(200).json(characeter))
+    .catch(err => res.json({ error: err}))
+})*/
+
+router.delete("/delete/:id", validateSession, function (req, res) {
+    const query = { where: { id: req.params.id, owner: req.user.id} };
+
+    characeter.destroy(query)
+    .then(() => res.status(200).json({ message: "Character Entry Removed"}))
+    .catch((err) => res.status(500).json({ error: err}));
+});
+
+
+module.exports = router;
