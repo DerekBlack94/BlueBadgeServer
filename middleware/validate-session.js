3e793d6ff6a1bd41fda8ab67e5887460a78ca2b8
Kelly Casper <72029749+krcasper@users.noreply.github.com>
2020-11-24 11:45:05 -0500

    Update validate-session.js
    Fixed a typo on line 49 ("undefined"

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -46,7 +46,7 @@ const validateSession = (req, res, next) => {
     } else if (req.headers.authorization) {
         const {authorization} = req.headers;
 
-        const payload = authorization ? jwt.verify(authorization, process.env.JWT_SECRET) : undifined;
+        const payload = authorization ? jwt.verify(authorization, process.env.JWT_SECRET) : undefined;
         console.log(payload);
 
         if(payload){
